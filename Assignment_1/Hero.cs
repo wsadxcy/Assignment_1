2bf67f8446a11dc7a681edd58ea9d90a44efb18a
Chenyuan Xie <1147719157xie@gmail.com>
2016-06-02 21:39:32 -0400

    Added version description

--- Assignment_1/Hero.cs
+++ Assignment_1/Hero.cs
@@ -4,8 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Assignment_1
 {
+    /**
+     * This class define a Hero
+     * 
+     * @class Hero
+     */
     class Hero
     {
     }
--- Assignment_1/Program.cs
+++ Assignment_1/Program.cs
@@ -4,10 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Author : Chenyuan Xie
+ * Date : June 2th, 2016
+ * Description : Assignment 1
+ * Version : 0.0.2 - Added version description
+ */
 namespace Assignment_1
 {
+    /**
+     * This class is the driver class for the program
+     * 
+     * @class program
+     */
     class Program
     {
+        /**
+         * the main method for my driver class
+         * 
+         * @method Main
+         * @param {sting[]} args
+         */
         static void Main(string[] args)
         {
         }
